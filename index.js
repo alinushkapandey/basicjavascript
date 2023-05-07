1808462b680746375d3889ad56d6b932afa86195
alinushka pandey <alinushkapandey99@gmail.com>
2023-05-07 17:33:50 +0545

    variables & type converison

---
+++ index.js
@@ -0,0 +1,57 @@
+//variables
+// var x=90;
+// x=120;
+// var x= 17+8;
+// console.log(x);
+//for let
+// let x=70;
+// console.log(x);
+// x=80;
+// console.log(x);
+// let x=100;
+// console.log(x);
+//const
+// const a=75;
+// console.log(a);
+// a=100;
+// console.log(a);
+// const a=45;
+// console.log(a);
+//data type
+// const y=90;
+// console.log(y);
+// console.log(typeof y);
+// const name="ali";
+// console.log(name);
+// console.log(typeof name);
+//another way of finding dTtype
+//const result="hari pradhan";
+// console.log(result);
+// console.log(typeof result);
+// console.log(typeof Number(result));
+// const num=12+14;
+// console.log(num);
+// console.log(typeof num);
+// console.log(typeof String(num));
+//boolean
+// let x=90;
+// let y=60;
+// let z=false;
+// console.log(x+y+z);
+// let x="harka rai";
+// let y=120;
+// console.log(x+y);
+// const name="ali";
+// const surname="pandey";
+// console.log("my name is : "+ name+ " " +surname);
+// console.log(`my name is : ${name} ${surname}.`);
+// let z;
+// console.log(z);
+// console.log(typeof z);
+// let z=null;
+// console.log(z);
+// console.log(typeof z);
+//string methods
+// const result ="my named is lod krishna!";
+// console.log(result[3]);
+
