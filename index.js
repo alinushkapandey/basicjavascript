ffcd11071694c58031c4734011f89b03aff23a2f
alinushka pandey <alinushkapandey99@gmail.com>
2023-05-10 17:17:23 +0545

    add statement & string method

--- index.js
+++ index.js
@@ -52,6 +52,141 @@
 // console.log(z);
 // console.log(typeof z);
 //string methods
-// const result ="my named is lod krishna!";
-// console.log(result[3]);
+//string method
+//const result ="my named is lod krishna!";
+//  length
+//  console.log (result.length);
+//concat
+// console.log(result.concat("yekdin tw sabbai janunai xa!"))
 
+//substring
+// console.log(result.substring(1,7));
+// console.log(result.substr(1,7));
+//repalce
+//console.log(result.replace("krishna", "shiva"));
+
+//includes
+//console.log(result.includes("mama"));
+//charat
+
+//console.log(result.charAt(7));
+
+//indexof
+//console.log(result.indexOf("l");
+
+//last index of
+//console.log(result.lastIndexOf("d"));
+
+//split
+//console.log(result.split(" "));
+
+//lowercase
+//console.log(result.toLowerCase());
+//uppercase
+//console.log(result.toUpperCase());
+
+//trim
+//console.log(result.trim());
+//statements(if else statements);
+
+//
+//const email="alinushkapandey99@gmail.com";
+//  const password ="8888888";
+//  if (email==="alinushkapandey99@gmail.com" && password==="8888888") {
+//     console.log("login sucessfully!");
+//  } else {
+//     console.log("invalid credetians!");
+
+//  }
+
+//simple arithmetic
+// let x=10;
+// if(x>15){
+//     console.log("xis greater than 15!");
+// }
+// else if(x>5){
+//     console.log("xis greater than 5,but not equals to 15!");
+// }else{
+//     console.log("x is not greater than 15!");
+// }
+// let weather= "cloudy";
+// if(weather=="sunny"){
+//     console.log("its a sunny day!");
+// }
+// else if (weather =="winter"){
+//     console.log("aja dherai jado xa hai!");
+// }
+// else if(weather=="rainy"){
+//     console.log("aaja pani parla jasto xa hai !");
+// }
+// else if (weather=="cloudy"){
+//     console.log("aaja  badal lay aakash dhakko xa !");
+
+// }
+// else{
+//     console.log("invalid weather !");
+// }
+
+//switch statement
+// let grade= "A";
+// switch (grade) {
+//     case "B":
+//         console.log("verry good marks");
+
+//         break;
+//         case "C":
+//             console.log("average marks");
+//         break;
+//          case "D":
+//             console.log("need to improve");
+//             break;
+//             case "A":
+//                 console.log("excellent marks");
+//                 break;
+//     default:
+//         console.log("fail vaya ");
+//         break;
+// }
+// let month = "March";
+// switch (month) {
+//   case "january":
+//   case "februrary":
+//   case "March":
+//     console.log("the month is in Q1");
+//     break;
+
+//   case "April":
+//   case "may":
+//   case "june":
+//     console.log("the month is in Q2");
+//     break;
+
+//   case "july":
+//   case "august":
+//   case "september":
+//     console.log("the month is in Q3");
+//     break;
+
+//   case "octomber":
+//   case "november":
+//   case "december":
+//     crossOriginIsolated.log("the month is in Q4");
+//     break;
+//   default:
+//     console.log(" vetiyena hai naresau!");
+// }
+  
+// let weather= "winter";
+// switch(weather){
+//     case "rainny":
+//         case "spring":
+//             console.log("the weather is in A1");
+//              break;
+
+//              case "summer":
+//                 case "winter":
+//              console.log("the weather is in A2");
+//              break;
+//               default:
+//                 console.log("weather ramro xaina");       
+//             }
