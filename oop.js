05f0e57a6bb74a3ab9f76bf318da549427206fac
alinushka pandey <alinushkapandey99@gmail.com>
2023-05-29 17:42:05 +0545

    inheritance

--- oop.js
+++ oop.js
@@ -1,9 +1,6 @@
 //oop in javascript
 
-//   function BankAccount(customerName, balance=0){
-//     this.customerName=customerName;
-//     this.accountNumber= Date.now();
-//      this.balance= balance;};
+
 //     //kunai pani property ma function add vayo vannya method hunxa;
 //     this.deposit= function(amount){
 //         this.balance+=amount;
@@ -77,3 +74,118 @@
 // sitaaccount.deposit(12000);
 // sitaaccount.withdraw(2000);
   
+//class in js
+
+// class BankAccount{
+//     constructor(customername, balance=0){
+//         this.customername=customername;
+//         this.accountnumber=Date.now();
+//         this.balance=balance;
+
+//     }
+
+//     deposite(amount){
+//         this.balance += amount;
+//     }
+//     withdraw(amount){
+//         this.balance -= amount;
+//     }
+// }
+// const ramaccount = new BankAccount("ram",4000);
+// ramaccount.deposite(4000);
+// const shyamaccount = new BankAccount("shyam",2000);
+// shyamaccount.deposite(12000);
+// shyamaccount.withdraw(500);
+// const sitaaccount = new BankAccount("sita",9000);
+// sitaaccount.deposite(3000);
+// sitaaccount.withdraw(1000);
+// console.log(ramaccount);
+// console.log(shyamaccount);
+// console.log(sitaaccount);
+
+
+// function Dog(name, breed){
+//     animal.call(this.name);
+//     this.breed = breed;
+// }
+// Dog.prototype = Object.create(animal .prototype);
+// Dog.prototype.bark= function(){
+//     console.log(`${this.name}barks.`);
+// }
+// const spot = new Dog("spot", "golden Retriever");
+// spot.speak();
+// spot.bark();
+// console.log(spot.breed);
+
+
+
+
+//inheritance 
+//super= it is used to inherit the properties of constructor
+
+
+// class BankAccount{
+//     #balance;
+    
+//     constructor(customername, balance=0){
+//         this.customername = customername;
+//      this.accountnumber = Date.now();
+//      this.#balance = balance;
+// }
+// deposite(amount){
+//     this.#balance += amount;
+// }
+
+// withdraw(amount){
+//     this.#balance -= amount;
+// }
+// setbalance(amount){
+//     this.#balance = amount;
+// }
+// getbalance(amount){
+//     return this.#balance;
+// }
+// }
+
+// class Savingaccount extends BankAccount{
+//     transactionslimit = 50000;
+//     constructor(customername, balance ){
+//         super(customername, balance);
+//     }
+//      #calculatingintrest(amount){
+//         console.log("calculating intrest is :" + amount);
+//      }
+//     takepersonalloan(amount){
+//         this.#calculatingintrest(amount);
+//         console.log("taking personalloan is"+amount);
+//     }
+// }
+// const aliaccount = new Savingaccount("ali",6000);
+// aliaccount.deposite(40000);
+
+// aliaccount.withdraw(2000);
+// console.log(aliaccount);
+
+
+
+// class currentaccount extends BankAccount{
+//     transactionslimit = 50000;
+//     constructor(customername, balance ){
+//         super(customername, balance);
+//     }
+//     takepersonalloan(amount){
+//         console.log("taking personalloan is"+amount);
+//     }
+// }
+// const parulaccount = new currentaccount("parl",6000);
+// parulaccount.deposite(40000);
+// parulaccount.withdraw(2000);
+// console.log(parulaccount);
+
+
+
+// const ramaccount = new BankAccount("ram",3000);
+// const hey = ramaccount.getbalance();
+// console.log(hey);
+// console.log(ramaccount);
+
